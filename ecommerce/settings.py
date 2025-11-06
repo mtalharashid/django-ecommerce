59b85e86a151d26a37be01b6d535a098b8eaf582
Talha Rashid <mtr04412@gmail.com>
2025-11-06 15:38:00 +0400

    Custom User model + Template setup
    Create custom user model for login by Email and create Roles
    
    Setup theme template

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "category",
+    "accounts",
+    
 ]
 
 MIDDLEWARE = [
@@ -69,6 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "ecommerce.wsgi.application"
 
+
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     
     
     path('', views.home, name='home')
