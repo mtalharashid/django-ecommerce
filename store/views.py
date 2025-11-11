6f09651f8c92be59db01435d75e454794708d8d1
Talha Rashid <mtr04412@gmail.com>
2025-11-11 15:40:27 +0400

    Setup store view

--- store/views.py
+++ store/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all().filter(is_available=True)
+    products_count = products.count()
+    context = {
+        'products': products,
+        'products_count': products_count,
+    }
+    return render(request, 'store/store.html', context)
